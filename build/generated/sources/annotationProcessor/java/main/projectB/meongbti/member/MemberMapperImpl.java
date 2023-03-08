7c5fc7e6595faa9f7e3c411e1d641644881885e8
Seoyoon <seoyoon047@gmail.com>
2023-03-08 21:13:23 +0900

    <chore>: merge_seoyoon_branch

---
+++ build/generated/sources/annotationProcessor/java/main/projectB/meongbti/member/MemberMapperImpl.java
@@ -0,0 +1,11 @@
+package projectB.meongbti.member;
+
+import javax.annotation.processing.Generated;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2023-03-06T09:24:50+0900",
+    comments = "version: 1.4.2.Final, compiler: IncrementalProcessingEnvironment from gradle-language-java-7.6.jar, environment: Java 11.0.17 (Azul Systems, Inc.)"
+)
+public class MemberMapperImpl implements MemberMapper {
+}
